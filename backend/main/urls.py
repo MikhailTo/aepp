8e7993cbe01b219303f37cadea7ae07f144911d3
MikhailTo <toshkin.mikhail@yandex.ru>
2023-11-10 22:26:44 +0300

    reorganization structure - one app and etc

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
 	path("", views.index, name="index"),
+	path("speed/", views.speed, name="speed"),
+	path("order/", views.order, name="order"),
+	path("location/", views.location, name="location"),
+	path("storage/", views.storage, name="storage"),
+	path("manuals/", views.manuals, name="manuals"),
 ]
