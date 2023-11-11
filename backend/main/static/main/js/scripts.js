2056917f8ea451ebe6924c0bf6fa9924db6401d7
MikhailTo <toshkin.mikhail@yandex.ru>
2023-11-11 10:10:33 +0300

    burger and sidebar - problem with display of sidebar in js

---
+++ backend/main/static/main/js/scripts.js
@@ -0,0 +1,6 @@
+const openSidebar = () => {
+    var sidebar = document.getElementById("sidebar")
+        (sidebar.style.display === "block") ?
+            sidebar.style.display = "none" :
+            sidebar.style.display = "block"
+}
