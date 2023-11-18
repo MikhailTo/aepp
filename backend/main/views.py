64b25d813090a4e24db70d56630ad58007f4862f
MikhailTo <toshkin.mikhail@yandex.ru>
2023-11-18 21:28:06 +0300

    get buckets with boto3 - experiment

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,6 +1,8 @@
 
 from django.shortcuts import render
 from main.forms import calcSpeedForm
+import boto3
+from decouple import config
 
 nav = [
     {'name': 'Главная',         'url': 'index'},
@@ -89,10 +91,36 @@ def storage(request):
     return render(request, 'main/storage.html', context=data)
 
 
+def getManuals(s3, bucket):
+    manuals = []
+    for key in s3.list_objects(Bucket=bucket)['Contents']:
+        if key['Key'][0:13] == 'data/manuals/' and key['Key'][-3:] == 'pdf':
+            name = ' '.join(key['Key'][:-4].split('/')[2:])
+            url = 'https://storage.yandexcloud.net/aepp.ru/' + key['Key']
+            manuals.append({'name': name, 'url': url})
+    return manuals
+
+
 def manuals(request):
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net',
+        region_name=config('REGION_NAME'),
+        aws_access_key_id=config('ACCESS_KEY'),
+        aws_secret_access_key=config('SECRET_KEY')
+    )
+
+    bucket_name = 'aepp.ru'
+    manuals = getManuals(s3, bucket_name)
+
+
     data = {
         'name': 'manuals',
         'title': 'Руководства по эксплуатации',
         'nav': nav,
+        'manuals': manuals,
     }
+
+
     return render(request, 'main/manuals.html', context=data)
